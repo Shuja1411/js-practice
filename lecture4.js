4e731626073246f4dae2375da83c8e19da9fd345
Shuja1411 <hafizshuja204@gmail.com>
2025-01-30 02:10:39 +0500

    task 4

---
+++ lecture4.js
@@ -0,0 +1,16 @@
+//primitive data types in JS
+//short form (nn bb ss u)
+let a = null;
+let b = 66;
+let c = true;
+let d = BigInt(444)
+let e = "shuja"
+let f = Symbol("this is a nice symbol");
+let g = undefined;
+console.log(a,b,c,d,e,f,g)
+console.log(typeof c)
+console.log(typeof d)
+
+
+
+

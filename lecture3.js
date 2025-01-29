38c383b328a0defda512536a2fc5c8c2f2a604c5
Shuja1411 <hafizshuja204@gmail.com>
2025-01-30 01:43:31 +0500

    task 3

---
+++ lecture3.js
@@ -0,0 +1,11 @@
+let a=12
+console.log(a)
+
+    let b="shuja"
+    console.log(b)
+
+                    let c=null
+                    console.log(c)
+                                  
+                                  let d=undefined
+                                  console.log(d)

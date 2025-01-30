8988e1bce45f3a53b4b69878ae5c0fa714004e10
Shuja1411 <hafizshuja204@gmail.com>
2025-01-31 00:44:08 +0500

    task 8

---
+++ lecture8.js
@@ -0,0 +1,31 @@
+/*let age = 22
+if(age>20 && age<10){
+console.log("you dosent drive the car");
+}
+else if (age>20 || age==15) {
+
+    console.log("you drive the car")
+}*/
+/*let age = 12
+switch(age){
+case 12:
+console.log("your age is 12")
+break;
+case 13: 
+console.log("your age is 13")
+break;
+case 14:
+console.log("your age is 14")
+break;
+}
+    console.log("your age is special")*/
+    let num=22
+    if(num% 2==0 && num% 3==0)
+        console.log("your no is divide by 2" );
+    else{
+console.log("your number is not divide by 2");
+
+    }
+    let age =3
+    let a= age>20 ? "you drive" : "you dont drive"
+    console.log(a)

da64907b57bbe4cd4a62e95ea9b72a44ac8427d1
Shuja1411 <hafizshuja204@gmail.com>
2025-01-30 13:25:48 +0500

    task5

---
+++ lecture5.js
@@ -0,0 +1,19 @@
+// craete variable of string
+let a ="shuja"
+let b=99
+console.log (a+b)
+// find type of data
+console.log (typeof (a+b))
+//find const
+const a1= {
+    name:"shuja",
+     age : 21,
+     principal : false,
+
+}
+//a1=45
+
+console.log(a1)
+a1["friend"]="ahmad"
+a1["name"]="ali"
+console.log(a1)

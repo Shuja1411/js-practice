6513289ee8bb22f93b7a49dbe6339bcf6fa4988e
Shuja1411 <hafizshuja204@gmail.com>
2025-01-30 17:26:15 +0500

    task 6

---
+++ lecture6.js
@@ -0,0 +1,39 @@
+// airthematic operator
+ a = 10;
+ b = 6;
+ console.log("a+b=", a+b)
+console.log("a-b=", a-b)
+console.log("a*b=", a*b)
+console.log("a/b=", a/b)
+console.log("a%b=", a%b)
+console.log("a**b=", a**b)
+console.log("a++=",a++)
+console.log("a--=",a--)
+
+// assignment operator 
+let assignment = 3;
+// assignment += 4;
+// assignment -= 1;
+// assignment *= 4;
+// assignment /=3;
+// assignment %= 2;
+// assignment **=3;
+console.log(assignment);
+
+// comparison operator
+let a1 = 4;
+let b1 = 3;
+// console.log(a1==b1)
+/*console.log("a1!=b1=",a1!=b1)
+console.log("a1>b1=",a1>b1)
+console.log("a1<b1=",a1>b1)*/
+// console.log("a1===b1=",a1===b1)
+// console.log("a1>=b1=",a1>=b1)
+
+//  logical operator
+let c= 22;
+let d = 7;
+console.log(c > d && c == d);
+console.log(c < d || c == d);
+console.log(!false)
+

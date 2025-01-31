864930153b04fa22c8dd0ac2d3ee48ef3c6113f6
Shuja1411 <hafizshuja204@gmail.com>
2025-01-31 19:08:57 +0500

    task 9

---
+++ lecture9.js
@@ -0,0 +1,15 @@
+let obj= {
+subhan: 22,
+ali : 11,
+ahmad : 55,
+}
+for (let a in obj){
+
+    console.log("marks of " + a +  "are"  + obj[a])
+}
+ for (let b in "hafiz") {
+
+    console.log(b)
+ }
+
+

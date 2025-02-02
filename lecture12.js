0f7dcccc0b1727cd83a16280f72725a59905ae33
Shuja1411 <hafizshuja204@gmail.com>
2025-02-02 18:47:31 +0500

    task 12

---
+++ lecture12.js
@@ -0,0 +1,12 @@
+// let fruit = `Bana\`na`
+// console.log(fruit)
+
+
+// let fruit =`man\ngo`
+// console.log(fruit)
+
+// let fruit =`man\tgo`
+// console.log(fruit)
+
+// let fruit =`man\`go`
+// console.log(fruit.length)

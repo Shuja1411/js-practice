d83f1a6012ccfdc0f70877897c5a2a0f567e1d0a
Shuja1411 <hafizshuja204@gmail.com>
2025-02-02 19:12:43 +0500

    task 13

---
+++ lecture13.js
@@ -0,0 +1,10 @@
+let class1 =[11,44,55,66,32]
+console.log(class1[2]) 
+class1[6]= 33
+
+console.log(class1[0])
+console.log(class1[6])
+
+class1[2]=22
+console.log(class1[2]) 
+console.log(class1.length)

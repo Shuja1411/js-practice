4c6053095c91b35f3f1f99dd9f2c746369d3468a
Shuja1411 <hafizshuja204@gmail.com>
2025-02-02 23:46:49 +0500

    task 14

---
+++ lecture14.js
@@ -0,0 +1,14 @@
+let num=[1,8,90,77]
+// console.log(num.sort ((a,b)=>a-b))
+
+// let r=num.shift()
+// console.log(r,num)
+
+// let r=num.unshift(55)
+// console.log(r,num)
+
+  let p=num.push(22)
+  console.log(p,num)
+
+ let r=num.pop()
+ console.log(num,r)

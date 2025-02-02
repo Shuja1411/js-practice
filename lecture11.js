4f4f3d3c43f35cd09160fc3818bff315dd1a8447
Shuja1411 <hafizshuja204@gmail.com>
2025-02-02 15:06:26 +0500

    task 11

---
+++ lecture11.js
@@ -0,0 +1,17 @@
+
+let a= 4
+let b= 5
+let c= 2
+
+/*function oneplusaverage(x,y){
+return 1+ (x+y) /2 (done)
+
+}*/
+const sum=(p,q)=>{
+
+     return p+q
+}
+
+console.log(sum (9,7))
+console.log("one plus average", (a,b))
+console.log("one plus average", (b,c))

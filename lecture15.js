8ccd0bb732c66fbf8f88d935358fb0f8d8353dae
Shuja1411 <hafizshuja204@gmail.com>
2025-02-03 18:13:09 +0500

    task 15

---
+++ lecture15.js
@@ -0,0 +1,31 @@
+// let num =[1, 2, 3, 4, 5, 6, 7, 8, 9  ]
+//  let num2 = num.map((x) => x * 2)
+//  console.log(num2)
+
+// delete num[3]
+// console.log(num)
+
+
+// let num_even =[22, 66, 44, 33, 22, 11]
+// let newarray =num.concat(num , num_even)
+// console.log(newarray)
+
+// num.sort()
+// console.log(num)
+
+// let compare=(a ,b) =>{
+    //     return(a-b)
+    //    }
+    
+    /*let compare=(b ,a) =>{
+        return(b-a)
+        }
+        num.sort(compare)
+        console.log(num)*/
+        
+        /*num.splice(2,3,2222,333,1111)
+        console.log(num)*/
+let num =[33,22,11,888,2,3,12]
+        
+let newNum=num.slice(2)
+console.log(newNum)

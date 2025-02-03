a30b4cc81fa309e50567dd2859467fa416f7aabc
Shuja1411 <hafizshuja204@gmail.com>
2025-02-03 23:28:39 +0500

    task 17

---
+++ lecture17.js
@@ -0,0 +1,33 @@
+
+/*let arr=[4,6,3,]
+let a=arr.map((index,array,value) =>{
+    console.log(index,array,value)
+return value
+
+
+})
+console.log(a)*/
+
+
+// let arr2=[4,6,3,9,22,33,11,99]
+// let arr=arr2.filter((a) =>{
+// return a>10
+
+
+// })
+// console.log(a)
+
+
+let arr1=[4,6,3,1]
+let arr=arr1.reduce((a1,a2)=>{
+ return(a1+a2)
+
+})
+console.log(arr)
+
+
+
+
+
+
+

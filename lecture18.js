3d33056dc10397b962369cbb2fb4cde0882b7aef
Shuja1411 <hafizshuja204@gmail.com>
2025-02-04 00:24:04 +0500

    task 18

---
+++ lecture18.js
@@ -0,0 +1,25 @@
+// let arr=[2,4,5,6,89,7]
+// arr.push("4")
+// let a1 = "shuja"
+//  let a=Number.parseInt(a1)
+// console.log(a)
+
+// let arr1=[3,4,7,2,3,9,99]
+// let a=arr1.reduce((a1,a2) =>{
+// return (a1+a2)
+
+// })
+// console.log(a)
+
+
+let arr1=[3,4,7,2,3,9,99]
+let a=arr1.filter((a) =>{
+return (a>5)
+
+})
+console.log(a)
+
+
+
+
+
